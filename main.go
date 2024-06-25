735a3208abe2c22ce51da203034d9c02bb19f554
ntauth0rity <nt.auth0rity@proton.me>
2024-06-25 11:17:44 -0600

    add concurrency based on cpu count

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"runtime"
 	"sync"
 	"time"
 )
@@ -25,19 +26,56 @@ func main() {
 		} else {
 			maxPort = 1024
 		}
-		for i := 1; i <= maxPort; i++ {
-			address := fmt.Sprintf("%s:%d", *addressFlag, i)
 
-			conn, err := net.DialTimeout("tcp", address, time.Duration(*timeoutFlag)*time.Second)
-			if err != nil {
-				if *verboseFlag {
-					fmt.Printf("Port %d closed.\n", i)
-				}
-				continue
-			} else {
-				fmt.Printf("Port %d open.\n", i)
-				conn.Close()
+		ports := make(chan int, maxPort)
+		results := make(chan int, maxPort)
+		numWorkers := runtime.NumCPU()
+
+		for i := 0; i < numWorkers; i++ { // using a limited number of goroutines
+			wg.Add(1)
+			go worker(*addressFlag, *timeoutFlag, *verboseFlag, ports, results, &wg)
+		}
+
+		go func() {
+			for j := 1; j <= maxPort; j++ {
+				ports <- j
+			}
+			close(ports) // close "ports" channel
+		}()
+
+		go func() { // close "results" channel when all workers are done
+			wg.Wait()
+			close(results)
+		}()
+
+		for port := range results {
+			if port != 0 {
+				fmt.Printf("Port %d open.\n", port)
 			}
 		}
+
+	} else {
+		fmt.Println("Please provide an address to be scanned using the -a flag.")
+		return
+	}
+}
+
+func worker(address string, timeout int, verbose bool, ports, results chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for port := range ports {
+		addr := fmt.Sprintf("%s:%d", address, port)
+		conn, err := net.DialTimeout("tcp", addr, time.Duration(timeout)*time.Second)
+
+		if err != nil {
+			if verbose {
+				fmt.Printf("Port %d closed.\n", port)
+			}
+			results <- 0 // Send 0 to indicate the port is closed
+			continue
+		}
+
+		conn.Close()
+		results <- port
 	}
 }
